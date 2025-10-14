bf083efcfdf0108b4097e6240ba97761da5e1a05
salpelter <dasaturov31@gmail.com>
2025-10-14 18:43:43 +0400

    Add comments to the sample plugin and make some minor adjustments for learning purposes

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+// TODO: rename classes and interfaces
 
 interface MyPluginSettings {
 	mySetting: string;
@@ -13,22 +13,22 @@ const DEFAULT_SETTINGS: MyPluginSettings = {
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
 
+	// onload() runs whenever the user starts using the plugin in Obsidian, this is where you'll configure most of the plugin's capabilities
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (_evt: MouseEvent) => {
-			// Called when the user clicks the icon.
+		// creates an icon in the left ribbon that shows a notice on click
+		const ribbonIconEl = this.addRibbonIcon('dice', 'Show a notice', (_evt: MouseEvent) => {
 			new Notice('This is a notice!');
 		});
-		// Perform additional things with the ribbon
+
 		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
+		// adds a status bar item to the bottom of the app (does not work on mobile)
 		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		statusBarItemEl.setText('Sample status bar text');
 
-		// This adds a simple command that can be triggered anywhere
+		// adds a simple command that can be triggered anywhere
 		this.addCommand({
 			id: 'open-sample-modal-simple',
 			name: 'Open sample modal (simple)',
@@ -36,7 +36,8 @@ export default class MyPlugin extends Plugin {
 				new SampleModal(this.app).open();
 			}
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
+
+		// adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
 			id: 'sample-editor-command',
 			name: 'Sample editor command',
@@ -45,39 +46,45 @@ export default class MyPlugin extends Plugin {
 				editor.replaceSelection('Sample Editor Command');
 			}
 		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
+
+		// adds a complex command that can check whether the current state of the app allows execution of the command
 		this.addCommand({
 			id: 'open-sample-modal-complex',
 			name: 'Open sample modal (complex)',
 			checkCallback: (checking: boolean) => {
-				// Conditions to check
+
+				// conditions to check
 				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
+
 				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
+
+					// when checking is true, we're simply "checking" if the command should even be run in the current circumstances
+					// when checking is false, then we actually perform the operation
 					if (!checking) {
 						new SampleModal(this.app).open();
 					}
 
-					// This command will only show up in Command Palette when the check function returns true
+					// the command will only show up in Command Palette when the first preliminary check function call returns true (available)
 					return true;
 				}
+				return false; // unavailable (optional, falls through as undefined otherwise)
 			}
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
+		// adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
+		// if the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin),
+		// using this function will automatically remove the event listener when this plugin is disabled.
 		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+			console.log('A click has just been triggered.', evt); // this will be called any time a click event happens on the document
 		});
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		// when registering intervals, this function will automatically clear the interval when the plugin is disabled
+		this.registerInterval(window.setInterval(() => console.log('5 minute interval has passed.'), 5 * 60 * 1000));
 	}
 
+	// onunload() runs when the plugin is disabled, any resources that your plugin is using must be released here to avoid affecting the performance of Obsidian after your plugin has been disabled
 	onunload() {
 
 	}
@@ -97,12 +104,12 @@ class SampleModal extends Modal {
 	}
 
 	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		const { contentEl } = this;
+		contentEl.setText('This is a sample modal!');
 	}
 
 	onClose() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.empty();
 	}
 }
@@ -116,7 +123,7 @@ class SampleSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
